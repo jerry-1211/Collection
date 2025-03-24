f3010c5df34cc81f2095f513e7c3e9d4f79fb397
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-03-24 19:49:24 +0900

    List & Tree 정렬

---
+++ src/collection/compare/SortMain4.java
@@ -0,0 +1,31 @@
+package collection.compare;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class SortMain4 {
+    public static void main(String[] args) {
+        MyUser myUser1 = new MyUser("a",30);
+        MyUser myUser2 = new MyUser("b",20);
+        MyUser myUser3 = new MyUser("c",10);
+
+        LinkedList<MyUser> list = new LinkedList<>();
+        list.add(myUser1);
+        list.add(myUser2);
+        list.add(myUser3);
+        System.out.println("기본 데이터");
+        System.out.println(list);
+
+        System.out.println("Comparable 기본 정렬");
+        list.sort(null);
+        // Collections.sort(list);
+        System.out.println(list);
+
+        System.out.println("IdComparator");
+        list.sort(new Idcomparator());
+        System.out.println(list);
+
+
+    }
+}
