134b37586c5062ebaf4819a1b92d0aeadd940a5e
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-03-24 16:38:04 +0900

    iterable, iterator 간단 구현

---
+++ src/collection/iterable/MyArrayIterator.java
@@ -0,0 +1,24 @@
+package collection.iterable;
+
+import java.util.Iterator;
+
+// Iterator 반복 가능하다는 것을 의미
+public class MyArrayIterator implements Iterator<Integer> {
+
+    private int currentIndex = -1;
+    private int[] targetArr;
+
+    public MyArrayIterator(int[] targetArr) {
+        this.targetArr = targetArr;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < targetArr.length - 1;
+    }
+
+    @Override
+    public Integer next() {
+        return targetArr[++currentIndex];
+    }
+}
